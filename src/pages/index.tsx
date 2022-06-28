60e045bcab488cbcee848e29c3611a9dd3e3eb4f
Adam Turner <adamjamesturner93@gmail.com>
2022-06-28 16:31:46 +0100

    Release landing page only

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,20 @@ export default function Home() {
   return (
     <AppTemplate className="w-full grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-4">
       <div className="flex flex-col gap-3 justify-center">
-        <h1 className="text-center text-4xl font-semibold">
+        <h1 className="text-center text-3xl sm:text-4xl font-semibold">
           Hi, I&apos;m Shruti Turner
         </h1>
-        <p className="text-center text-lg font-medium">
-          Yogi | PhD | Data Scientist
+        <p className="text-center sm:text-lg sm:font-medium">
+          Yogi | PhD | Machine Learning Engineer
         </p>
       </div>
-      <figure className="relative aspect-square w-full mx-auto max-w-xs rounded-full overflow-hidden md:max-w-md lg:max-w-lg">
-        <Image src="https://picsum.photos/500" alt="temp" layout="fill" />
+      <figure className="relative aspect-square w-full mx-auto max-w-xs overflow-hidden md:max-w-md lg:max-w-lg">
+        <Image
+          src="/profile.jpeg"
+          alt="Shruti Turner"
+          layout="fill"
+          className="object-contain"
+        />
       </figure>
     </AppTemplate>
   );
