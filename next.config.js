27f77fb4a72bea309db2dfb9bfc9552d3ea7cac8
Adam Turner <adamjamesturner93@gmail.com>
2022-06-28 16:37:47 +0100

    simplify setup

--- next.config.js
+++ next.config.js
@@ -1,25 +1,13 @@
-const STUDIO_REWRITE = {
-  source: "/studio/:path*",
-  destination:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3333/studio/:path*"
-      : "/studio/index.html",
-      
-};
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites: () => [STUDIO_REWRITE],
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
   },
   images: {
     domains: ['picsum.photos', 'cdn.sanity.io'],
   },
+};
 
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
