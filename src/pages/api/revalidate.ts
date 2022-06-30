8128591614f1b9a586a74d3541232c26df152ebf
Adam Turner <adamjamesturner93@gmail.com>
2022-06-30 11:35:48 +0100

    added api to invalidate data

---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,43 @@
+import { isValidRequest } from '@sanity/webhook';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  message: string;
+};
+
+const secret = process.env.SANITY_WEBHOOK_SECRET;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  if (req.method !== 'POST') {
+    console.error('Must be a POST request');
+    return res.status(401).json({ message: 'Must be a POST request' });
+  }
+
+  if (!secret) {
+    return res.status(500).json({ message: 'Invalid setup' });
+  }
+
+  if (!isValidRequest(req, secret)) {
+    return res.status(401).json({ message: 'Invalid signature' });
+  }
+
+  try {
+    const { body } = req;
+    return res.json(body);
+
+    // switch (type) {
+    //   case 'post':
+    //     await res.unstable_revalidate(`/news/${slug}`);
+    //     return res.json({
+    //       message: `Revalidated "${type}" with slug "${slug}"`,
+    //     });
+    // }
+
+    // return res.json({ message: 'No managed type' });
+  } catch (err) {
+    return res.status(500).send({ message: 'Error revalidating' });
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,6 +46,5 @@ export const getStaticProps: GetStaticProps<Props> = async ({
     props: {
       data,
     },
-    revalidate: 10,
   };
 };
