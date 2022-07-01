1822e4e0c1791ec686c255b6acc6ecc143f1fe52
Adam Turner <adamjamesturner93@gmail.com>
2022-07-01 07:34:54 +0100

    return req

--- api/revalidate.ts
+++ api/revalidate.ts
@@ -1,4 +1,4 @@
-import { isValidRequest } from '@sanity/webhook';
+// import { isValidRequest } from '@sanity/webhook';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
@@ -20,26 +20,28 @@ export default async function handler(
     return res.status(500).json({ message: 'Invalid setup' });
   }
 
-  return res.json({ message: JSON.stringify({ secret, req }) });
+  return res
+    .status(200)
+    .json({ message: `secret: ${secret}, req: ${JSON.stringify(req)}}` });
 
-  if (!isValidRequest(req, secret)) {
-    return res.status(401).json({ message: 'Invalid signature' });
-  }
+  // if (!isValidRequest(req, secret)) {
+  //   return res.status(401).json({ message: 'Invalid signature' });
+  // }
 
-  try {
-    const { body } = req;
-    return res.json(body);
+  // try {
+  //   const { body } = req;
+  //   return res.json(body);
 
-    // switch (type) {
-    //   case 'post':
-    //     await res.unstable_revalidate(`/news/${slug}`);
-    //     return res.json({
-    //       message: `Revalidated "${type}" with slug "${slug}"`,
-    //     });
-    // }
+  // switch (type) {
+  //   case 'post':
+  //     await res.unstable_revalidate(`/news/${slug}`);
+  //     return res.json({
+  //       message: `Revalidated "${type}" with slug "${slug}"`,
+  //     });
+  // }
 
-    // return res.json({ message: 'No managed type' });
-  } catch (err) {
-    return res.status(500).send({ message: 'Error revalidating' });
-  }
+  // return res.json({ message: 'No managed type' });
+  // } catch (err) {
+  //   return res.status(500).send({ message: 'Error revalidating' });
+  // }
 }
